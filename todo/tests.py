440dc40ed5da58a01933921cdee6feccf5a16d8e
1F10220106 <s1f102201065@iniad.org>
2023-07-06 15:31:30 +0900

    警告を修正３

--- todo/tests.py
+++ todo/tests.py
@@ -21,7 +21,6 @@ class TaskModelTestCase(TestCase):
         self.assertEqual(task.title, 'task1')
         self.assertFalse(task.completed)
         self.assertEqual(task.due_at, due)
-    
 
     def test_create_task2(self):
         task = Task(title='task2')
@@ -32,15 +31,13 @@ class TaskModelTestCase(TestCase):
         self.assertFalse(task.completed)
         self.assertEqual(task.due_at, None)
 
-
     def test_is_overdue_future(self):
         due = timezone.make_aware(datetime(2023, 6, 30, 23, 59, 59))
         current = timezone.make_aware(datetime(2023, 6, 30, 0, 0, 0))
-        task=Task(title='task1', due_at=due)
+        task = Task(title='task1', due_at=due)
         task.save()
 
         self.assertFalse(task.is_overdue(current))
-
 
     def test_is_overdue_past(self):
         due = timezone.make_aware(datetime(2023, 6, 30, 23, 59, 59))
@@ -49,7 +46,6 @@ class TaskModelTestCase(TestCase):
         task.save()
 
         self.assertTrue(task.is_overdue(current))
-
 
     def test_is_overdue_none(self):
         due = (None)
@@ -69,7 +65,6 @@ class TodoViewTestCase(TestCase):
         self.assertEqual(response.templates[0].name, 'todo/index.html')
         self.assertEqual(len(response.context['tasks']), 0)
 
-
     def test_index_post(self):
         client = Client()
         data = {'title' : 'Test Task', 'due_at' : '2023-06-30 23:59:59'}
@@ -78,7 +73,6 @@ class TodoViewTestCase(TestCase):
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response.templates[0].name, 'todo/index.html')
         self.assertEqual(len(response.context['tasks']), 1)
-
 
     def test_index_get_order_post(self):
         task1 = Task(title='task1', due_at=timezone.make_aware(datetime(2023, 7, 1)))
@@ -93,7 +87,6 @@ class TodoViewTestCase(TestCase):
         self.assertEqual(response.context['tasks'][0], task2)
         self.assertEqual(response.context['tasks'][1], task1)
     
-
     def test_index_get_order_due(self):
         task1 = Task(title = 'task1', due_at = timezone.make_aware(datetime(2023, 7, 1)))
         task1.save()
